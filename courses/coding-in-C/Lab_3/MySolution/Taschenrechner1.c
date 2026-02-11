15c357a1dee7a7f060e3aeb279ef2758db487839
emoyap06-art <emoyap06@gmail.com>
2026-02-11 10:20:54 +0000

    Scanf- Befehl überarbeitet

--- courses/coding-in-C/Lab_3/MySolution/Taschenrechner1.c
+++ courses/coding-in-C/Lab_3/MySolution/Taschenrechner1.c
@@ -8,39 +8,50 @@ char operator;
 float ergebnis;
 
     printf("Geben Sie die erste Zahl ein: ");
-    scanf("%f", &nummer1);
-    printf(" Zahl 1 ist: %f\n", nummer1);
-    //if (scanf("%f", &nummer1) != 1) {
-    //    printf("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.\n");
-    //    return 1; // Beendet das Programm mit einem Fehlercode
-    //}
 
-    printf("Geben Sie die zweite Zahl ein: ");
-    scanf("%f", &nummer2);
+      if (scanf(" %f", &nummer1) != 1) {
+       printf("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.\n");
+        return 1; // Beendet das Programm mit einem Fehlercode
+      }
+    printf(" Zahl 1 ist: %f\n", nummer1);  
+
+
+
+    printf("Geben Sie die zweite Zahl ein: \n");
+
+     if (scanf(" %f", &nummer2) != 1) {
+        printf("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.\n");
+        return 1; // Beendet das Programm mit einem Fehlercode
+     }
     printf(" Zahl 2 ist: %f\n", nummer2);
-    //if (scanf("%f") != 1) {
-    //    printf("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.\n");
-    //    return 1; // Beendet das Programm mit einem Fehlercode
-    //}
+
+
+
     printf("Wählen Sie nun einen dieser Oparatoren aus: +, -, *, / \n");    
     scanf(" %c", &operator);
+
+
 
     if(operator == '+') {
         ergebnis= nummer1 + nummer2;
     
     } else if(operator== '-') {
         ergebnis = nummer1 - nummer2;
+
     } else if (operator == '*') {
         ergebnis = nummer1 * nummer2;
+        
     } else if (operator == '/') {
-        ergebnis= nummer1 / nummer2;
         if (nummer2 == 0) {
             printf("Mathematischer Fehler!\n");
             return 1; // Beendet das Programm mit einem Fehlercode
+        } else {
+        ergebnis= nummer1 / nummer2;
+        
         }
     }
 
-    printf("Das Ergebnis ist= %f\n", ergebnis);
+    printf("Das Ergebnis ist= %.2f \n", ergebnis);
 
     return 0;
 }
